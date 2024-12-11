bf3446359005c08a81943bd86c6d7b8b941bbb88
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-11 10:40:58 +0530

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,5 +1,5 @@
 let a=7
-
+// shortcut to create an array and then finding the factorial of the number
 function factorial(number){
     let arr=Array.from(Array(number+1).keys())
     let c=arr.slice(1,).reduce((a,b)=>{
