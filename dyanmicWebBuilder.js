2efd79b1d994ce6d68b9202806873a2af03ce88d
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-13 17:47:26 +0530

    dynamic website builder

---
+++ dyanmicWebBuilder.js
@@ -0,0 +1,28 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    let viewStr
+    if(views<1000){
+        viewStr = views
+    }
+    else if(views>1000000){
+        viewStr = views/1000000 + "M"
+    }
+    else{
+        viewStr=views/1000 + "K"
+    }
+    let html = `<div class="card">
+            <div class="image">
+                <img src="${thumbnail}" alt="">
+                <div class="capsule">${duration}</div>
+            </div>
+            <div class="text">
+                <h1>${title}</h1>
+                <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+            </div>
+        </div>`
+    document.querySelector(".container").innerHTML += html     
+}
+
+createCard("Introduction to Back end | Sigma Web Dev video #2", "CodeWithHarry", 73000000, 7, "20:04", "1.jpg")
+createCard("Introduction to Node Js | Sigma Web Dev video #3", "CodeWithHarry", 6500000, 7, "22:33", "1.jpg")
+createCard("Introduction to PHP | Sigma Web Dev video #4", "CodeWithHarry", 69, 7, "37:12", "1.jpg")
+createCard("Introduction to MySQL | Sigma Web Dev video #5", "CodeWithHarry", 100000, 7, "30:02", "1.jpg")
